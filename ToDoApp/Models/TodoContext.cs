cd32f782021c70d58798c3b645867b06cdac24d6
Pauliina <Pauliina@AWA5CD2116DNV>
2023-02-28 11:29:42 +0200

    Add project files.

---
+++ ToDoApp/Models/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ToDoApp.Models
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public virtual DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ ToDoApp/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<TodoContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoAPi")));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyAllowedOrigins",
+                    policy =>
+                    {
+                        policy
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseCors("MyAllowedOrigins");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
